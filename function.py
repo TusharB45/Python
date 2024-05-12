35b9d8699ccb88004ccf50e9bf6cdc48da7381b2
Borse Tushar Mukesh <122999869+TusharB45@users.noreply.github.com>
2024-05-12 19:54:51 +0530

    Update function.py

--- function.py
+++ function.py
@@ -1,3 +1,16 @@
 def simple():
     print ("My first function")
 simple()
+# Using a Function in Another Function
+def plus_five(x):
+    return x + 5
+
+def m_by_3(x):
+    return plus_five(x) * 3
+#Another Way to Define a Function
+def multiplication_by_2(x):
+    result = x ** 2
+    print (x, "Raised to the power of 2:")
+    return result
+    
+multiplication_by_2(5)
