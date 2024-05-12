d6478718bb3ffa4733a2ac6b6b584e3ffc14f0c3
Borse Tushar Mukesh <122999869+TusharB45@users.noreply.github.com>
2024-05-12 20:03:08 +0530

    Create Dictionaries.py

---
+++ Dictionaries.py
@@ -0,0 +1,11 @@
+Menu = {'meal_1':'Spaghetti', 'meal_2':'Fries', 'meal_3':'Hamburger', 'meal_4':'Lasagna'}
+Menu['meal_2']
+Menu['meal_2']
+Price_list = {}
+Price_list[Menu['meal_1']] = 10
+Price_list[Menu['meal_2']] = 5
+Price_list[Menu['meal_3']] = 8
+Price_list[Menu['meal_4']] = 12
+Price_list[Menu['meal_5']] = 5
+Price_list
+Price_list.get('Spaghetti')
