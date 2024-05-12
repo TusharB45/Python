04587414536de4d412983fed2ffa92e33ebc53eb
Borse Tushar Mukesh <122999869+TusharB45@users.noreply.github.com>
2024-05-12 20:05:04 +0530

    Create Loops.py

---
+++ Loops.py
@@ -0,0 +1,16 @@
+digits = [0,1,2,3,4,5,6,7,8,9]
+for d in digits:
+    print (d)
+  
+for d in digits:
+    print (d, end=" ")
+
+x = 1
+while x <= 30:
+    print (x, end=" ")
+    x = x + 2
+  
+x = 1
+while x <= 30:
+    print (x, end=" ")
+    x += 2
